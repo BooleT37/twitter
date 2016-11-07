380d331089ae3e0e49b37bc0e82b97e5c8dfe74a
Алексей Левин <BooleT37@gmail.com>
2016-11-07 20:41:12 +0500

    Added Rest controller

--- src/main/java/ru/urfu/controllers/MessagesController.java
+++ src/main/java/ru/urfu/controllers/MessagesController.java
@@ -1,46 +1,22 @@
 package ru.urfu.controllers;
 
-import java.util.Map;
-
-import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestOperations;
 import org.springframework.web.servlet.ModelAndView;
 import ru.urfu.models.Message;
-import ru.urfu.storage.TemporalStorage;
-import ru.urfu.storageManager.IStorageManager;
-import ru.urfu.storageManager.TemporalStorageManager;
-import ru.urfu.storageManager.exceptions.StorageManagerException;
 
-import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
 
-import static org.springframework.web.bind.annotation.RequestMethod.POST;
-
-/**
- * @author aarkaev
- * @since 08.08.2016
- */
 @Controller
 public class MessagesController {
-
-    private final TemporalStorage storage = TemporalStorage.getInstance();
-    private final IStorageManager storageManager = new TemporalStorageManager(storage);
-
+    @Autowired
+    private RestOperations operations;
 
     @RequestMapping("/messages")
     public ModelAndView messages() {
-        Map<Long, Message> messages = storageManager.getAllMessages();
-        return new ModelAndView("messages", "messagesList", messages);
-    }
-
-    @RequestMapping(value = "/addMessage", method = POST)
-    public void addMessage(@RequestParam(value = "content") String content, HttpServletResponse response) {
-        try {
-            storageManager.addMessageWithUniqId(new Message(content));
-        } catch (StorageManagerException e) {
-            response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
-            e.printStackTrace();
-        }
+        Map<Long, Message> messages = operations.getForObject("/getAllMessages", Map.class);
+        return new ModelAndView("messages", "messages", messages);
     }
 }
--- src/main/webapp/WEB-INF/jsp/messages.jsp
+++ src/main/webapp/WEB-INF/jsp/messages.jsp
@@ -1,4 +1,4 @@
-<%@ page contentType="text/html;charset=UTF-8" language="java" import="ru.urfu.message.Message,java.util.ArrayList,java.util.List" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" import="ru.urfu.models.Message,java.util.Map" %>
 <%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html>
 
@@ -8,8 +8,8 @@
         <h1>twitter</h1>
         This is your twitter application
         <ul class="messages">
-            <c:forEach var="message" items="${messagesList}">
-                <c:out value="${message.getContent()}"/> <br/>
+            <c:forEach var="message" items="${messages}">
+                <c:out value="${message.getValue().getContent()}"/> <br/>
             </c:forEach>
         </ul>
     </body>
