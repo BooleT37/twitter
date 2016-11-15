2837abe5c8297167d197614f629c6627ef9109ce
Alexey Levin <BooleT37@gmail.com>
2016-11-15 16:57:16 +0500

    Modified TwitterApplication file to support deployment

--- src/main/java/ru/urfu/TwitterApplication.java
+++ src/main/java/ru/urfu/TwitterApplication.java
@@ -1,11 +1,17 @@
 package ru.urfu;
 
+import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TwitterApplication {
-
+public class TwitterApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(TwitterApplication.class, args);
 	}
